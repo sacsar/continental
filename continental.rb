fae6b1fed6ef701fcc66ddc34687ce9ad0ac77cb
sacsar <sacsar@gmail.com>
2013-08-26 19:32:51 -0500

    Everything basically works

--- continental.rb
+++ continental.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'slim'
-require 'data_mapper'
+#require 'data_mapper'
 
 enable :sessions
+set :session_secret, '12310afldskjf220194'
 
 #=begin
 #DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
@@ -18,20 +19,22 @@ end
 #DataMapper.finalize
 
 get '/' do
-  session["names"] ||= nil
+  session.clear
   slim :index 
 end
 
 post '/' do
   @player = params[:players].to_i
   @decks = (@player + 1 )/2
+  session[:decks] = @decks
+  session[:players] = []
   slim :players
 end
 
 post '/names' do
   session[:players] = Array.new
   params[:names].each do |id|
-    session[:players].push(Player.new(id))
+    session[:players].push(Player.new(id.last))
   end
   session[:round]=1
   redirect to('/round')
@@ -40,5 +43,19 @@ end
 get '/round' do
   @round = session[:round]
   @players = session[:players]
+  @decks = session[:decks]
+  session[:round] = @round + 1
   slim :round
+end
+
+post '/round' do
+  players = session[:players]
+  output = ""
+  players.each_index do |i|
+    j = i.to_s
+    score = 5*(params[:others][i.to_s].to_i) + 10*(params[:tens][i.to_s].to_i+params[:jacks][i.to_s].to_i+params[:queens][i.to_s].to_i+params[:kings][i.to_s].to_i) + 20*params[:aces][i.to_s].to_i + 50*params[:jokers][i.to_s].to_i
+    players[i].score = players[i].score.to_i + score.to_i
+  end
+  session[:players] = players
+  redirect to('/round')
 end
